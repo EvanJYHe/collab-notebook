8bcf1913a1f65ff9e2da11cdfd3646345aada615
Evan He <evan.he24@gmail.com>
2025-02-05 21:50:57 -0500

    initialized mongodb and skeleton of project

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL)
+    .then((result) => console.log("connected to db"))
+    .catch((err) => console.log(err))
+
